0d894f57e5893a362c92c7e15ed4b4af5885a1e5
bchong95 <bschong2@illinois.edu>
2015-07-29 03:35:55 +0000

    using jQuery datatables to fix the header and first column

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,46 @@
 contestApp.controller('ListCtrl', ['$scope', '$http', function ($scope, $http) {
-    $http.get('./data/results.json').success(function(data) {
-      $scope.students = data;
+    $http.get('./data/results.json').success(function(students) {
+      $scope.students = students;
+      var table = $("<table></table>");
+      var thead = $("<thead></thead>");
+      var headRow = $("<tr></tr>");
+      headRow.append($("<th>Nickname</th>"));
+      for (var i = 0; i < students[0].test_cases.length; i++) {
+        var testcase = students[0].test_cases[i];
+        headRow.append($("<th>"+testcase.name+"</th>"));
+      }
+      headRow.append($("<th>Timestamp</th>"));
+      thead.append(headRow);
+      table.append(thead);
+      var tbody = $("<tbody></tbody>");
+      // var tbody = $("");
+      for (var i = 0; i < students.length; i++){
+        var student = students[i];
+        var tBodyRow = $("<tr></tr>");
+        tBodyRow.append($("<th>"+student.nickname+"</th>"));
+        var testcases = student.test_cases;
+        for (var j = 0; j < testcases.length; j++) {
+          var testcase = testcases[j];
+          tBodyRow.append($("<td>"+testcase.pts_earned+"/"+testcase.total_pts+" pts "+"</td>"))
+        }
+        tBodyRow.append($("<td>"+student.time_stamp+"</td>"));
+        tbody.append(tBodyRow);
+      }
+      table.append(tbody);
+      $("#content").append(table);
+      console.log(table);
+      var dataTable = table.DataTable( {
+            scrollY:        "500px",
+            scrollX:        true,
+            scrollCollapse: true,
+            paging:         false
+        } );
+
+      new $.fn.dataTable.FixedColumns( dataTable, {
+          leftColumns: 1,
+          // rightColumns: 1
+      } );
+      $(window).resize();
     });
 
     $scope.options = [
