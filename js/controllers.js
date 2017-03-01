cd76b0ec3c7bfcb268864760355015871937f77a
Ben Kurtovic <ben.kurtovic@gmail.com>
2017-02-28 22:22:30 -0600

    Fix hardcoding, fudging.

--- js/controllers.js
+++ js/controllers.js
@@ -1,111 +1,14 @@
 contestApp.controller('ContestCtrl', ['$scope', '$http', function ($scope, $http) {
     $http.get('./data/results.json').success(function(students) {
-      // Hard coding TA statistics to stop students from bleeding too hard
-      var ta = {
-          "total_time": 33.517323999999995,
-          "test_cases": [
-            {
-              "avg_memory": 1000000.0,
-              "total_pts": 1.0,
-              "name": "part2_tester-1",
-              "max_memory": 1000000.0,
-              "pts_earned": 1.0,
-              "runtime": 5.988813
-            },
-            {
-              "avg_memory": 167724872.411363,
-              "total_pts": 1.0,
-              "name": "part2_tester-2",
-              "max_memory": 209686528.0,
-              "pts_earned": 1.0,
-              "runtime": 3.73751
-            },
-            {
-              "avg_memory": 1048401.266456,
-              "total_pts": 1.0,
-              "name": "part2_tester-3",
-              "max_memory": 1048576.0,
-              "pts_earned": 1.0,
-              "runtime": 2.031576
-            },
-            {
-              "avg_memory": 397814286.35719,
-              "total_pts": 1.0,
-              "name": "part2_tester-4",
-              "max_memory": 1073741824.0,
-              "pts_earned": 1.0,
-              "runtime": 1.519419
-            },
-            {
-              "avg_memory": 262339907.839119,
-              "total_pts": 1.0,
-              "name": "part2_tester-5",
-              "max_memory": 518926336.0,
-              "pts_earned": 1.0,
-              "runtime": 0.604985
-            },
-            {
-              "avg_memory": 790393287.111111,
-              "total_pts": 1.0,
-              "name": "part2_tester-6",
-              "max_memory": 1789571072.0,
-              "pts_earned": 1.0,
-              "runtime": 8.847705
-            },
-            {
-              "avg_memory": 447391744.0,
-              "total_pts": 1.0,
-              "name": "part2_tester-7",
-              "max_memory": 1073741824.0,
-              "pts_earned": 1.0,
-              "runtime": 3.639624
-            },
-            {
-              "avg_memory": 853773568.0,
-              "total_pts": 1.0,
-              "name": "part2_tester-8",
-              "max_memory": 2028179456.0,
-              "pts_earned": 1.0,
-              "runtime": 4.956268
-            },
-            {
-              "avg_memory": 348122438.176778,
-              "total_pts": 1.0,
-              "name": "part2_tester-9",
-              "max_memory": 655773696.0,
-              "pts_earned": 1.0,
-              "runtime": 0.292566
-            },
-            {
-              "avg_memory": 167833995.785251,
-              "total_pts": 1.0,
-              "name": "part2_tester-10",
-              "max_memory": 268636160.0,
-              "pts_earned": 1.0,
-              "runtime": 1.639981
-            },
-            {
-              "avg_memory": 1207929353.766156,
-              "total_pts": 1.0,
-              "name": "part2_tester-11",
-              "max_memory": 2147483648.0,
-              "pts_earned": 1.0,
-              "runtime": 0.258877
-            }
-          ],
-          "is_ta_solution": true,
-          "last_revision": "40795",
-          "total_pts_earned": 11.0,
-          "total_max_memory": 9766789120.0,
-          "time_stamp": "2016-03-05 13:05:38.873592",
-          "nickname": "glibc",
-          "total_avg_memory": 4644371854.713424
-        }
-
-      // Swapping out ta with hardcoding:
+      // Find TA solution and fudge some values:
+      var ta;
       for (var i = 0; i < students.length; i++) {
           if(students[i].is_ta_solution) {
-            students[i] = ta;
+            ta = students[i];
+            ta["nickname"] = "glibc";
+            ta["test_cases"][0]["max_memory"] += 32.;
+            ta["test_cases"][0]["avg_memory"] += 32.;
+            break;
           }
       }
 
@@ -119,16 +22,7 @@ contestApp.controller('ContestCtrl', ['$scope', '$http', function ($scope, $http
           if(student.test_cases == undefined) {
             console.log(student)
           }
-          // for (var i = 0; i < student.test_cases.length; i++) {
-          //     var testcase =  student.test_cases[i];
-          //     if (testcase.pts_earned != testcase.total_pts) {
-          //       return -1;
-          //     }
-          //     totalMaxMemory += Number(testcase.max_memory);
-          //     totalAvgMemory += Number(testcase.avg_memory);
-          //     totalRuntime += Number(testcase.runtime);
-          // }
-          // return .6 * Number(totalMaxMemory) + .2 * Number(totalAvgMemory) + .2 * Number(totalRuntime);
+
           // http://stackoverflow.com/questions/4856717/javascript-equivalent-of-pythons-zip-function
           function zip(arrays) {
               return arrays[0].map(function(_,i){
